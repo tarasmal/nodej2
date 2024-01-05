38fbb74b23fdabd9dc0a6a6fc9bd84ab5995355e
tarasmaliarchuk <tarasmalarcuk8@gmail.com>
2024-01-05 12:41:09 +0200

    add routes

--- index.js
+++ index.js
@@ -14,6 +14,18 @@ router.post("/example/:id", async (req, res, payload) => {
   res.writeHead(200, { "Content-Type": "application/json" });
   res.end(JSON.stringify({ message: "POST request to /example", payload }));
 });
+
+router.get('/user', (req, res, payload) => {
+  console.log(payload);
+  res.writeHead(200, { "Content-Type": "text/plain" });
+  res.end("GET request to /user");
+})
+
+router.put('/user', (req, res, payload) => {
+  console.log(payload)
+  res.writeHead(200, { "Content-Type": "application/json" });
+  res.end(JSON.stringify({ message: "PUT request to /user", payload }));
+})
 console.log(router);
 
 const server = http.createServer((req, res) => router.handleRequest(req, res));
